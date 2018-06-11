b442dcbd9fad464cd0f2129d93c33d4efc0b7d57
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2018-06-11 20:48:44 +0200

    new event function

--- Ass 2/solution w2 Jesse.py
+++ Ass 2/solution w2 Jesse.py
@@ -16,22 +16,8 @@ import os
 os.chdir("C:/Users/Jesse/OneDrive/Bureaublad laptop Jesse/Pre-master/Project big data/data-week-1")
 
 
-def read_csv_data(filenames):
-    
-    
-    textfile='hue_upload.csv'
-    colnames = ['row id','user id','event id','value']
-    data = pd.read_csv(textfile,sep = ';'
-                       ,header=None
-                       ,names=colnames)
-    
-    def find_event(string):
-        return re.findall(r'[a-zA-Z]+[_]*[a-zA-Z]+',string)
-    
-    x=pd.Series(index=[i for i in range(0,len(data))])
-    
-
-    
+def read_csv_data(filenames):   
+   
 #%%  
     def getInformativeEvents(data):
         searchfor = ['lamp_change','nudge_time','bedtime_tonight','risetime','rise_reason','adherence_importance', 'fitness']     
@@ -43,14 +29,16 @@ def read_csv_data(filenames):
     def addEvent(data):
         l = []
         
-        searchfor = ['lamp_change','nudge_time','bedtime_tonight','risetime','rise_reason','adherence_importance', 'fitness']           
-        for i in data2.index:
-            if(searchfor[0] in data2['event id'][i]):
-                data2['event'] == 'lamp_change'
-            elif(searchfor[1] in data2['event id'][i]):
-                data2['event'] == 'nudge_time'                         
-            else: 'no event'
-                    ]
+        for i in range(0,len(data)):
+            string = data['event id'][i]
+            matches = re.search(r'(lamp_change|nudge_time|bedtime_tonight|risetime|rise_reason|adherence_importance|fitness)',string)
+            
+            l.append(matches.group(0) if matches else l.append('No event'))
+            
+        df = pd.DataFrame(l)
+        df.columns = ['event']
+        
+        return pd.merge(df,data,left_index=True,right_index=True)         
         
 #%%    
     def addDateTime(data):       
@@ -58,30 +46,39 @@ def read_csv_data(filenames):
         for i in range(0,len(data)):
             string = data['event id'][i]
             matches = re.search(r'\d{1,2}_\D{1,12}_\d{4}',string)
-            if matches:
-                l.append(matches.group(0))
-            else: l.append('No date')
+            
+            l.append(matches.group(0) if matches else l.append('No date'))
         
-        df = pd.DataFrame(v)
+        df = pd.DataFrame(l)
         df.columns = ['Datetime']
         
         return pd.merge(df,data,left_index=True,right_index=True)
+    
+    #%%
+    
+    def createID(data):
+        data.set_index(['Datetime'],['event'])       
+        return data
+
+#%%
+    
+    textfile='hue_upload.csv'
+    colnames = ['row id','user id','event id','value']
+    data = pd.read_csv(textfile,sep = ';'
+                       ,header=None
+                       ,names=colnames) 
+    
+    data2=getInformativeEvents(data)
+    
+    data2 = addEvent(data2)
+    
+    data2 = addDateTime(data2)
+
+    data3= createID(data2)    
+    
+    
+    
 #%%    
-    k = addDateTime(data2)
-    
-    ##Line per line
-    import re as re
-    #read line of file
-    with open(textfile) as f:
-        flines = f.readlines()
-        
-    for line in flines:
-        match = re.search(r'',line)
-        if match:
-            coordinateList.append(readCoordinate(match.group(1)))
-            pieceList.append(match.group(3))
-        else:next         
-    
     
 def to_mongodb(df):
     None
@@ -98,8 +95,5 @@ if __name__ == '__main__':
     df = read_csv_data(["hue_upload.csv","hue_upload2.csv"])
     # to_mongodb(df)
     # read_mongodb({},'_id')
-
-
-
 
 
