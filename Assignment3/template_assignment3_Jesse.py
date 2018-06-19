b3168019144772979636fd71f6d736799f114fa9
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2018-06-19 08:48:41 +0200

    Ex 3 added

--- Assignment3/template_assignment3_Jesse.py
+++ Assignment3/template_assignment3_Jesse.py
@@ -13,16 +13,15 @@ Jesse Schouten j7.schouten@student.vu.nl
 # Include these lines without modifications
 # Call the script as follows: ./<scriptname> <csv_filename> <mapper function> <reducer function>
 # So, for example: ./template_assignment3.py hue_week_3_2017.csv mapper1 reducer1
-# This will call the mapper1 function for each line of the data, sort the output, and feed the sorted output into reducer1
+# This will call the mapper1 function for each line of the data, sort the output, and feed the sorted output into import sys
 import sys
+import numpy as np
 from io import StringIO
 import traceback
 from queue import Queue
 import requests
 import json
 import threading
-
-
 
 # Implement these mapper and reducer functions
 
@@ -49,12 +48,97 @@ def mapper2(line):
 def reducer2(line):
     None
 
-def mapper3(line):
-    None
+def mapper3(line):  
+    def convertToDatetime(datestring):
+        import datetime       
+        import re as re
+        
+        matches = re.search(r'((\d{4})-(\d{2})-(\d{2}) (\d{1,2}):(\d{1,2}):(\d{1,2}).(\d{3}))+', datestring)
+        
+        year = int(matches.group(2))
+        month = int(matches.group(3))
+        day = int(matches.group(4))
+        hour = int(matches.group(5))
+        minute = int(matches.group(6))
+        second = int(matches.group(7))
+        millisecond = int(matches.group(8))
+        
+        result = datetime.datetime(year, month, day, hour, minute, second, millisecond)
+        
+        return result
+    
+    def convertTimeToSeconds(time):      
+        import re as re
+        
+        matches = re.search(r'(\d{1,2}):(\d{1,2}):(\d{1,2}).(\d{3})+', time)
+        
+        hour = int(matches.group(1))
+        minute = int(matches.group(2))
+        second = int(matches.group(3))
+        
+        result = (hour*60*60)+(minute*60)+(second)
+        
+        return result
+    
+    def shiftHalfOfADay(timeInSeconds):
+        #The goal is to shift a time in seconds to midday to calculate an average
+        if timeInSeconds < 43200 and timeInSeconds > 0:
+            #shift up if after midnight
+            timeInSeconds += 43200
+        elif timeInSeconds >= 43200 and timeInSeconds < 86400:
+            #shift down if before midnight
+            timeInSeconds -= 43200
+        return timeInSeconds
+
+    user = line.split(',')[1]
+    
+    bedtime = line.split(',')[2]
+    
+    if bedtime:   
+        #bedtimeAsDatetime = convertToDatetime(bedtime)
+        dayInSeconds = convertTimeToSeconds(bedtime)
+        shiftedDayInSeconds = shiftHalfOfADay(dayInSeconds)
+        
+        print('{}\t{}'.format(user, shiftedDayInSeconds))
 	
 def reducer3(line):
-    None
+    def ShiftSecondsBack(seconds):
+        #The goal is to shift a time in seconds to midday to calculate an average
+        if seconds < 43200:
+            #shift up if after afternoon
+            seconds += 43200
+        elif seconds >= 43200:
+            #shift down if before afternoon
+            seconds -= 43200
+        return seconds
+    
+    def secondsToTime(seconds):
+        import datetime
+        return str(datetime.timedelta(seconds=seconds))
 
+    global sum_time, current_user, user, count                 
+  
+    if line:
+        user, time = line.split('\t')
+        time = float(time)
+    
+        if(current_user == user):
+            sum_time += time
+            count = count + 1
+        else: 
+            if current_user != None:
+                mean_shifted_time = round(sum_time/count, 2)
+                mean_time = ShiftSecondsBack(mean_shifted_time)
+                print('{}\t{}'.format(current_user, secondsToTime(mean_time)))
+            count = 1
+            sum_time = time
+            current_user = user
+    else:
+        mean_shifted_time = round(sum_time/count, 2)
+        mean_time = ShiftSecondsBack(mean_shifted_time)
+        print('{}\t{}'.format(current_user, secondsToTime(mean_time)))
+
+    
 def mapper4(line):
     None
 
@@ -74,6 +158,16 @@ def process_queue(queue):
 def main():
     None	
 
+global count
+global sum_time
+count, sum_time = 0, 0
+
+global current_user
+global user
+global current_count
+current_user, user = None, None
+current_count = 0
+
 import os
 os.chdir("C:/Users/Jesse/OneDrive/Bureaublad laptop Jesse/Pre-master/Project big data")
 
@@ -86,8 +180,8 @@ if(len(sys.argv) == 4):
     reducer = sys.argv[3]
 else:
     data = 'hue_week_3_2017.csv'
-    mapper = 'mapper1'
-    reducer = 'reducer1'
+    mapper = 'mapper3'
+    reducer = 'reducer3'
 
 # Include these lines without modifications
  
@@ -96,13 +190,16 @@ if 'old_stdout' not in globals():
 mystdout = StringIO()
 sys.stdout = mystdout
 
-
 with open(data) as file:
+    #lines = [line.rstrip('\n') for line in file]
+    #for line in lines: 
+     #   line_values = line.split(';')
     try:
         for index, line in enumerate(file):
             if index == 0:
                 continue
             line = line.strip()
+            
             locals()[mapper](line)
         locals()[mapper](',,,,,,,') 
     except:
