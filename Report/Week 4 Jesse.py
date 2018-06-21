4646ae3f40807c2e84838114112cec69b764b91c
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2018-06-21 16:00:32 +0200

    progress on assigment 4

--- Report/Week 4 Jesse.py
+++ Report/Week 4 Jesse.py
@@ -14,12 +14,6 @@ This template is meant as a guideline. Feel free to alter existing functions and
 Remember to use descriptive variable names and to keep functions concise and readable.
 """
 
-import os
-os.chdir("C:/Users/Jesse/Documents/Github/Project_Big_Data/Report")
-
-sleepdatafile   = 'hue_week_4_2017.csv'
-surveydatafile  = 'hue_questionnaire.csv'
-
 """
 The main() function is called when template_week_4.py is run from the command line.
 It is a good place to define the logic of the data flow (for example, reading, transforming, analyzing, visualizing).
@@ -27,22 +21,77 @@ It is a good place to define the logic of the data flow (for example, reading, t
 
 
 def main():
-    None
-
-def read_data(sleepdatafile, surveydatafile): 
+    import os
+    os.chdir("C:/Users/Jesse/Documents/Github/Project_Big_Data/Report")
+    
     import pandas as pd
     import numpy as np
     
+    sleepdatafile   = 'hue_week_4_2017.csv'
+    surveydatafile  = 'hue_questionnaire.csv'
     week4Data = pd.read_csv(sleepdatafile,delimiter = ';')
-    
+    week4Data = week4Data.set_index('ID')
     questionnaireData = pd.read_csv(surveydatafile, delimiter = ',')
-
-    delBedtimeCount = week4Data.iloc[:,5]
-
-    mergedData = week4Data[['ID','Condition']]
+    questionnaireData = questionnaireData.set_index('ID')
     
-
+    newDataframe = createNewDataframe(sleepdatafile,questionnaireData)
     
+    mergeDataframes(questionnaireData,newDataframe)
+
+def createNewDataframe(sleepdatafile, surveydatafile): 
+    def isBiggerThenZero(x):
+        if x > 0:
+            return 1
+        else:
+            return 0
+    
+    #source:https://stackoverflow.com/questions/944700/how-can-i-check-for-nan-in-python
+    def isnan(value):
+      try:
+          import math
+          return math.isnan(float(value))
+      except:
+          return False
+    
+    def insert_if_new(df,idx):
+        if idx not in df.index:
+            df = df.append(pd.Series({'group' : float('nan'),\
+                                      'delay_nights' : float('nan'),\
+                                      'delay_time' : float('nan')},\
+                                      name=idx))
+        return df
+    
+    columns = ['group','delay_nights','delay_time']
+    dataresult = pd.DataFrame(columns=columns)
+    
+    for index, row in week4Data.iterrows():
+        index = row["ID"] 
+        if index not in dataresult:
+            dataresult = insert_if_new(dataresult,index)
+        condition = row[1]
+        dataresult = dataresult.set_value(index,'group',condition)
+        
+        delayNights=0
+        for i in [5,15,25,35,45,55,65,75,85,95,105,115]:
+            delayNights += isBiggerThenZero(row[i]) 
+        dataresult = dataresult.set_value(index,'delay_nights',delayNights)
+        
+        delayTime = 0.0
+        days = 0
+        for i in [5,15,25,35,45,55,65,75,85,95,105,115]:
+            if isBiggerThenZero(row[i]) == 1:
+                delayTime += row[i]
+                if not isnan(row[i]):
+                    days +=1
+        if days != 0:
+            dataresult = dataresult.set_value(index,'delay_time',int(round(delayTime/days)))
+        
+    return dataresult
+
+def mergeDataframes(frame1,frame2):
+    import pandas as pd
+    
+    return pd.merge(frame1,frame2, how = 'inner',left_index = True,right_index=True)
     
 def correlate(x, y, test_type = 'pearson'):
     None
