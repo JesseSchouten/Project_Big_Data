6127b3270d39cd4189c88725b74ac77b70276b9e
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2018-06-23 15:46:17 +0200

    updated files

--- Report/Week 4 Jesse.py
+++ Report/Week 4 Jesse.py
@@ -24,6 +24,8 @@ os.chdir("C:/Users/Jesse/Documents/Github/Project_Big_Data/Report")
 import pandas as pd
 import numpy as np
 import scipy.stats as ss
+import statsmodels.api as sm
+import matplotlib.pyplot as plt 
 
 def createNewDataframe(sleepdatafile, surveydatafile): 
     def isBiggerThenZero(x):
@@ -69,8 +71,8 @@ def createNewDataframe(sleepdatafile, surveydatafile):
         for i in [5,15,25,35,45,55,65,75,85,95,105,115]:
             if isBiggerThenZero(row[i]) == 1:
                 delayTime += row[i]
-                if not isnan(row[i]):
-                    days +=1
+            if not isnan(row[i]):
+                days +=1
         if days != 0:
             dataresult = dataresult.set_value(index,'delay_time',int(round(delayTime/days)))
         
@@ -81,10 +83,16 @@ def mergeDataframes(frame1,frame2):
     
     return pd.merge(frame1,frame2, how = 'inner',left_index = True,right_index=True)
     
-def correlate(x, y, test_type = 'pearson'):
+def correlate(x, y, test_type):
     x = np.array(x)
     y=np.array(y)
-    r,p = ss.pearsonr(x,y)
+
+    if test_type == 'pearson':
+        r,p = ss.pearsonr(x,y)
+    elif test_type == 'kendall':
+        r,p = ss.kendalltau(x,y)
+    else: 
+        return 'Choose valid test method'
     
     return [r,p]
 
@@ -94,9 +102,12 @@ def compare(x, y, test_type = 'ttest'):
 def regress(target, predictors):
     None
     
+#def visualize():
     
-sleepdatafile   = 'hue_week_4_2017.csv'
-surveydatafile  = 'hue_questionnaire.csv'
+
+    
+sleepdatafile = 'hue_week_4_2017.csv'
+surveydatafile = 'hue_questionnaire.csv'
 week4Data = pd.read_csv(sleepdatafile,delimiter = ';')
 questionnaireData = pd.read_csv(surveydatafile, delimiter = ',')
 
@@ -108,13 +119,100 @@ mergedData = mergeDataframes(questionnaireData,newDataframe)
 
 x = mergedData[['bp_scale','delay_time']].dropna()['bp_scale']
 y = mergedData[['bp_scale','delay_time']].dropna()['delay_time']
-correlate(x,y)
+correlate(x,y,'pearson')
+
+x = mergedData[['age','delay_time']].dropna()['age']
+y = mergedData[['age','delay_time']].dropna()['delay_time']
+correlate(x,y,'kendall')
+
+x = mergedData[['daytime_sleepiness','delay_time']].dropna()['daytime_sleepiness']
+y = mergedData[['daytime_sleepiness','delay_time']].dropna()['delay_time']
+correlate(x,y,'pearson')
+
+
+#Histogram of delay nights
+print(mergedData.query('group==1')['delay_nights'].mean())
+print(np.std(mergedData.query('group==1')['delay_nights']))
+print(np.median(mergedData.query('group==1')['delay_nights']))
+
+print(mergedData.query('group==0')['delay_nights'].mean())
+print(np.std(mergedData.query('group==0')['delay_nights']))
+print(np.median(mergedData.query('group==0')['delay_nights']))
+
+x=mergedData['delay_nights'].dropna().astype(int)
+plt.hist(x, label = 'Delay nights')
+plt.legend(loc = 'upper right')
+plt.suptitle("Histogram of delay nights")
+plt.xlabel('Number of delay nights')
+plt.ylabel('Frequency')
+plt.show()
+
+#Histogram of time participants spent in bed each night
+    #the condition column in week4Data is the 'group' in the mergedData file
+def getBedtimes(query):     
+    result = []    
+        #source:https://stackoverflow.com/questions/944700/how-can-i-check-for-nan-in-python
+    def isnan(value):
+      try:
+          import math
+          return math.isnan(float(value))
+      except:
+          return False
+    
+    for row_index,row in query.iterrows():
+        for i in [7,17,27,37,47,57,67,77,87,97,107,117]:
+            if not isnan(row[i]):
+                result.append(row[i]) 
+           
+    return result
+        
+
+queryGroup0 = week4Data.query('Condition==0')
+queryGroup1 = week4Data.query('Condition==1')
+
+sleeptimesGroup0 = pd.DataFrame(getBedtimes(queryGroup0))
+sleeptimesGroup0.columns = ['Inbedtimes']
+sleeptimesGroup1 = pd.DataFrame(getBedtimes(queryGroup1))
+sleeptimesGroup1.columns = ['Inbedtimes']
+
+print(sleeptimesGroup0.mean())
+print(np.std(sleeptimesGroup0))
+print(np.median(sleeptimesGroup0))
+
+print(sleeptimesGroup1.mean())
+print(np.std(sleeptimesGroup1))
+print(np.median(sleeptimesGroup1))
+
+
+#Histogram of mean time participants spent delaying their bedtime
+print(mergedData.query('group==1')['delay_time'].mean())
+print(np.std(mergedData.query('group==1')['delay_time']))
+print(np.median(mergedData.query('group==1')['delay_time']))
+
+print(mergedData.query('group==0')['delay_time'].mean())
+print(np.std(mergedData.query('group==0')['delay_time']))
+print(np.median(mergedData.query('group==0')['delay_time']))
+
+y= mergedData['delay_time'].dropna().astype(int)
+plt.hist(y, label = 'Delay time')
+plt.legend(loc = 'upper right')
+plt.suptitle("Histogram of delay time")
+plt.xlabel('Delay time in seconds')
+plt.ylabel('Frequency')
+plt.show()
+
+results = smf.ols('delay_time ~ age + chronotype + motivation', data=mergedData).fit()
+    
+group1data = mergedData.groupby(['group'])
+
+
+
+def calculateDifferenceInGroups(df):
+    
 
 """
 Tip: create one function per visualization, and call those functions from the main visualize() function.
 """
-def visualize():
-    None
 
 
 def main():
